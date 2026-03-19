274b7c598d84221c10095a292c3475920644609b
Corentin546 <corentin.senez546@gmail.com>
2026-03-19 10:04:32 +0100

    Add files via upload

---
+++ Serveur/CServeur_Web.cpp
@@ -0,0 +1,32 @@
+///////////////////////////////////////////////////////////
+//  CServeur_Web.cpp
+//  Implementation of the Class CServeur_Web
+//  Created on:      05-mars-2026 09:51:36
+//  Original author: The Administrator
+///////////////////////////////////////////////////////////
+
+#include "CServeur_Web.h"
+
+
+CServeur_Web::CServeur_Web(){
+
+}
+
+
+
+CServeur_Web::~CServeur_Web(){
+
+}
+
+
+
+
+
+void CServeur_Web::Demande_Action(){
+
+}
+
+
+void CServeur_Web::Demande_IHM(){
+
+}
---
+++ Serveur/CServeur_Web.h
@@ -0,0 +1,22 @@
+///////////////////////////////////////////////////////////
+//  CServeur_Web.h
+//  Implementation of the Class CServeur_Web
+//  Created on:      05-mars-2026 09:51:35
+//  Original author: The Administrator
+///////////////////////////////////////////////////////////
+
+#if !defined(EA_67D144E0_7E2D_4a58_8403_AB0AD6BFFB14__INCLUDED_)
+#define EA_67D144E0_7E2D_4a58_8403_AB0AD6BFFB14__INCLUDED_
+
+class CServeur_Web
+{
+
+public:
+	CServeur_Web();
+	virtual ~CServeur_Web();
+
+	void Demande_Action();
+	void Demande_IHM();
+
+};
+#endif // !defined(EA_67D144E0_7E2D_4a58_8403_AB0AD6BFFB14__INCLUDED_)
