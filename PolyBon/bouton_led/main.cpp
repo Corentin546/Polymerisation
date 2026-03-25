e82406c192fec2e0a6e46d3b4bb06fa8887299c3
Corentin <corentin.senez546@gmail.com>
2026-03-25 17:30:29 +0100

    testv2

---
+++ PolyBon/bouton_led/main.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <unistd.h>
+
+int main() {
+    std::cout << "--- Demarrage du controle GPIO ---" << std::endl;
+
+    // 1. Preparation des pins (Export)
+    // On utilise le 23 pour le bouton et le 24 pour la LED
+    std::ofstream exp("/sys/class/gpio/export");
+    exp << "23";
+    exp.flush();
+    exp << "24";
+    exp.close();
+
+    // Pause pour laisser Linux creer les dossiers
+    usleep(200000);
+
+    // 2. Configuration des directions
+    std::ofstream("/sys/class/gpio/gpio23/direction") << "in";
+    std::ofstream("/sys/class/gpio/gpio24/direction") << "out";
+
+    std::cout << "Systeme pret ! (Appuyez sur le bouton)" << std::endl;
+
+    std::string etatBouton;
+
+    while (true) {
+        // LIRE le bouton (GPIO 23)
+        std::ifstream bouton("/sys/class/gpio/gpio23/value");
+        bouton >> etatBouton;
+        bouton.close();
+
+        // ECRIRE la valeur sur la LED (GPIO 24)
+        std::ofstream led("/sys/class/gpio/gpio24/value");
+        led << etatBouton;
+        led.close();
+
+        // Petit affichage pour confirmer dans la console
+        std::cout << "\rBouton : " << etatBouton << " -> LED : " << etatBouton << std::flush;
+
+        usleep(20000); // 20ms de delai pour ne pas surcharger le processeur
+    }
+
+    return 0;
+}
---
+++ PolyBon/testled2/main.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <pigpio.h>
+#include <unistd.h> // Pour la fonction sleep()
+
+// On définit le numéro du pin GPIO (schéma Broadcom / BCM)
+const int LED_PIN = 19;
+
+int main() {
+    // 1. Initialisation de la bibliothèque pigpio
+    if (gpioInitialise() < 0) {
+        std::cerr << "Erreur : Impossible d'initialiser pigpio" << std::endl;
+        return 1;
+    }
+
+    // 2. Configuration du pin en mode SORTIE (Output)
+    gpioSetMode(LED_PIN, PI_OUTPUT);
+
+    std::cout << "Allumage de la LED sur le pin " << LED_PIN << "..." << std::endl;
+
+    // 3. Allumer la LED (High / 1)
+    gpioWrite(LED_PIN, 1);
+
+    // 4. Attendre 10 secondes
+    sleep(10);
+
+    // 5. Éteindre la LED (Low / 0)
+    gpioWrite(LED_PIN, 0);
+    std::cout << "La LED est éteinte. Fin du programme." << std::endl;
+
+    // 6. Libérer les ressources
+    gpioTerminate();
+
+    return 0;
+}
